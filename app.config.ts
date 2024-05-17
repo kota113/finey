f06a07514647c5c82588c07c6e27425e8b780f5b
kota113 <kota113@keio.jp>
2024-05-17 15:19:21 +0900

    update version

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: "Finey",
         slug: "Finey",
-        version: "1.0.0",
+        version: "1.0.1",
         orientation: "portrait",
         icon: "./assets/icon.png",
         userInterfaceStyle: "light",
