5aa07e330f6baf344fc9ea6e03757795b786306e
kota113 <kota113@keio.jp>
2024-06-14 15:07:22 +0900

    modify to automatically rename dev builds

--- devBuild.sh
+++ devBuild.sh
@@ -3,3 +3,5 @@ export EAS_LOCAL_BUILD_ARTIFACTS_DIR=./builds
 GOOGLE_SERVICES_JSON=$(realpath ./creds/google-services.json)
 export GOOGLE_SERVICES_JSON
 eas build -p android --profile development --local
+# rename the latest modified file to dev.apk
+mv $(ls -t $EAS_LOCAL_BUILD_ARTIFACTS_DIR/*.apk | head -1) dev.apk
