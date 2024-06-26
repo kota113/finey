0cd60eb9b436c30151998cbdeda8652f78d440d3
kota113 <kota113@keio.jp>
2024-06-26 15:29:44 +0900

    add a button to change the payment method

--- screens/dialogs/PaymentFailed.tsx
+++ screens/dialogs/PaymentFailed.tsx
@@ -1,22 +1,28 @@
-import {Dialog, Portal, Text} from "react-native-paper";
+import {Button, Dialog, Portal, Text} from "react-native-paper";
 import React from "react";
 import {StyleSheet} from "react-native";
 
-export function PaymentFailedDialog({visible, setVisible}: {
+export default ({visible, setVisible, navigation}: {
     visible: boolean,
-    setVisible: React.Dispatch<React.SetStateAction<boolean>>
-}) {
+    setVisible: React.Dispatch<React.SetStateAction<boolean>>,
+    navigation: any
+}) => {
     return (
         <Portal>
             <Dialog visible={visible} onDismiss={() => setVisible(false)}>
                 <Dialog.Icon icon={"alert"} color={"#ff8c00"}/>
                 <Dialog.Title style={styles.dialogTitle}>お支払いに失敗しました</Dialog.Title>
                 <Dialog.Content>
-                    <Dialog.Content>
-                        <Text>ご利用の決済手段が無効となっています。</Text>
-                        <Text>別の決済手段をご登録ください。</Text>
-                    </Dialog.Content>
+                    <Text>ご利用の決済手段が無効となっています。</Text>
+                    <Text>別の決済手段をご登録ください。</Text>
                 </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={() => {
+                        setVisible(false);
+                        navigation.navigate("EditPayment");
+                    }}>決済手段を変更</Button>
+                    <Button onPress={() => setVisible(false)}>閉じる</Button>
+                </Dialog.Actions>
             </Dialog>
         </Portal>
     )
